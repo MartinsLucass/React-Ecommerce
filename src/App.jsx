865f0977acb89902637db0c63c47851d89193222
Lucas Martins <maritinslucas@gmail.com>
2023-07-06 16:35:29 -0300

    implementing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import React from "react"
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom"
 
-function App() {
+import Home from "./pages/Home";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return(
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {path: '/', element: <Home />}
+    ]  }
+])
+
+const App = () => {
   return (
-      <h1 className="text-red-200">
-        Hello world!
-      </h1>
+    <div>
+      <RouterProvider router={router}/>
+    </div>
 
   )
 }
